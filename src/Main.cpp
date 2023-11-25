9bc88dd9000c17f0676bf8455af37c03f36a1c5b
Jérôme Braëms <54836707+jeromeBRM@users.noreply.github.com>
2023-11-25 19:01:45 +0100

    include tools sfml

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,45 +1,39 @@
 // Libraries
 #include <reactphysics3d/reactphysics3d.h>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 // ReactPhysics3D namespace
 using namespace reactphysics3d; 
- 
+using namespace sf;
+
 // Main function 
-int main(int argc, char** argv) { 
- 
-    // First you need to create the PhysicsCommon object. 
-    // This is a factory module that you can use to create physics 
-    // world and other objects. It is also responsible for 
-    // logging and memory management 
-    PhysicsCommon physicsCommon; 
- 
-    // Create a physics world 
-    PhysicsWorld* world = physicsCommon.createPhysicsWorld(); 
- 
-    // Create a rigid body in the world 
-    Vector3 position(0, 20, 0); 
-    Quaternion orientation = Quaternion::identity(); 
-    Transform transform(position, orientation); 
-    RigidBody* body = world->createRigidBody(transform); 
- 
-    const decimal timeStep = 1.0f / 60.0f; 
- 
-    // Step the simulation a few steps 
-    for (int i=0; i < 20; i++) { 
- 
-        world->update(timeStep); 
- 
-        // Get the updated position of the body 
-        const Transform& transform = body->getTransform(); 
-        const Vector3& position = transform.getPosition(); 
- 
-        // Display the position of the body 
-        std::cout << "Body Position: (" << position.x << ", " << 
-      position.y << ", " << position.z << ")" << std::endl; 
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
     }
 
-    _sleep(2000);
- 
-    return 0; 
+    return 0;
 }
