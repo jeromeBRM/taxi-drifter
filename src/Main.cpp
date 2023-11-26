9c69fdf7fc48f05ff0c1c4e3fa280424f4339664
Jérôme Braëms <54836707+jeromeBRM@users.noreply.github.com>
2023-11-26 12:57:22 +0100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,23 +1,30 @@
 // Libraries
-#include <reactphysics3d/reactphysics3d.h>
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include <thread>
 #include <cassert>
 
-int main() {
-    sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "TaxiDrifter SFML debugger");
-    sf::Font font;
+#include <reactphysics3d/reactphysics3d.h>
+#include <SFML/Graphics.hpp>
 
-    if (!font.loadFromFile("myfont.ttf")) {
-        return -1;
+using namespace std;
+using namespace reactphysics3d;
+
+const decimal TIME_STEP = 1.0f / 60.0f;
+const decimal WINDOW_REFRESH_STEP = 1.0f / 144.0f * 1000;
+
+void runPhysics(PhysicsWorld* world) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds((int)std::floor(TIME_STEP * 1000)));
+        
+        world->update(TIME_STEP);
     }
-    sf::Text message(L"\u6f22\u5b57", font);
+}
 
-    sf::RectangleShape rectangle(sf::Vector2f(120, 50));
-
-    int pos = 0;
+void runGraphics(RigidBody* body) {
+    sf::RenderWindow sfmlWin(sf::VideoMode(1600, 900), "TaxiDrifter SFML debugger");
 
     while (sfmlWin.isOpen()) {
+        std::this_thread::sleep_for(std::chrono::milliseconds((int)std::floor(WINDOW_REFRESH_STEP)));
 
         sf::Event e;
         while (sfmlWin.pollEvent(e)) {
@@ -26,17 +33,34 @@ int main() {
             case sf::Event::EventType::Closed:
                 sfmlWin.close();
                 break;
-            case sf::Event::MouseMoved:
-                rectangle.setPosition(e.mouseMove.x, e.mouseMove.y);
-                break;
             }
-
         }
 
+        sf::RectangleShape rectangle(sf::Vector2f(50, 50));
+
+        rectangle.setPosition(200, - 100 - 70 * body->getTransform().getPosition().y);
+
         sfmlWin.clear();
-        sfmlWin.draw(message);
         sfmlWin.draw(rectangle);
         sfmlWin.display();
     }
+}
+
+int main() {
+    PhysicsCommon physicsCommon;
+
+    PhysicsWorld* world = physicsCommon.createPhysicsWorld();
+
+    Vector3 init_position(0, 20, 0);
+    Quaternion init_orientation = Quaternion::identity();
+    Transform init_transform(init_position, init_orientation);
+    RigidBody* body = world->createRigidBody(init_transform);
+
+    thread physics_thread = thread(runPhysics, world);
+    thread graphics_thread = thread(runGraphics, body);
+
+    physics_thread.join();
+    graphics_thread.join();
+
     return 0;
 }
